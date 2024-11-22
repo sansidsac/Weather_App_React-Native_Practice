cf8a048617e65100d511740b3d5028bda6a74f54
sansidsac <kssreekumar04@gmail.com>
2024-11-22 17:53:37 +0530

    Enhance City screen layout with background image and weather details

--- screens/City.tsx
+++ screens/City.tsx
@@ -1,15 +1,71 @@
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet } from 'react-native'
+import { SafeAreaView, Text, StyleSheet, ImageBackground, StatusBar, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
 
 const styles=StyleSheet.create({
     container:{
-        flex:1
+        flex:1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    imageLayout:{
+        flex:1,
+        resizeMode: "cover",
+    },
+    cityName:{
+        fontSize: 40,
+    },
+    countryName:{
+        fontSize: 30,
+        color: 'black'
+    },
+    cityText:{
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white',
+        padding: 10
+    },
+    popWrapper:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+    },
+    popText:{
+        fontSize: 25,
+        marginLeft: 7.5,
+        color: 'red',
+        fontWeight: 'bold'
+    },
+    riseSetWrapper:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 30
+    },
+    riseSetText:{
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
     }
 })
 const City = () => {
   return (
     <SafeAreaView style={styles.container}>
-        <Text>City</Text>
+        <ImageBackground source={require('../assets/images/city-bg.jpg')} style={styles.imageLayout}>
+            <Text style={[styles.cityName, styles.cityText]}>London</Text>
+            <Text style={[styles.cityText, styles.countryName ]}>UK</Text>
+            <View style={styles.popWrapper}>
+                <Feather name={'user'} size={50} color={'red'}/>
+                <Text style={styles.popText}>11000</Text>
+            </View>
+            <View style={styles.riseSetWrapper}>
+                <Feather name={'sunrise'} size={50} color={'white'}/>
+                <Text style={styles.riseSetText}>10:45:58am</Text>
+                <Feather name={'sunset'} size={50} color={'white'}/>
+                <Text style={styles.riseSetText}>17:28:23pm</Text>
+            </View>
+        </ImageBackground>
     </SafeAreaView>
   )
 }
