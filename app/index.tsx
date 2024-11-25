e2dc041637dd1e831fc9c2f847ad72426e7a7eae
sansidsac <kssreekumar04@gmail.com>
2024-11-26 00:12:29 +0530

    Implement bottom tab navigation for CurrentWeather, UpcominWeather, and City screens; update dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,17 @@
 import CurrentWeather from "@/screens/CurrentWeather";
 import City from "@/screens/City";
 import { UpcominWeather } from "@/screens/UpcominWeather";
-import {View, StyleSheet} from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const styles=StyleSheet.create({
-  container:{
-    flex:1,
-  }
-})
+const Tab = createBottomTabNavigator();
 
 export default function Index() {
   return (
-    <>
-      <View style={styles.container}>
-        <CurrentWeather />
-      </View>
-    </>
+    <Tab.Navigator>
+      <Tab.Screen name="CurrentWeather" component={CurrentWeather} />
+      <Tab.Screen name="UpcominWeather" component={UpcominWeather} />
+      <Tab.Screen name="City" component={City} />
+    </Tab.Navigator>
   );
 }
 
